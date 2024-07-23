96e6eca04ac609f26db5e87cd36d6b119f28757a
nurag-pradhan <pradhananurag2005@gmail.com>
2024-07-23 19:04:11 +0530

    new update

---
+++ login.js
@@ -0,0 +1,8 @@
+let popUpBox = document.getElementById("PopUp");
+
+function displaypopup() {
+    popUpBox.classList.add("display");
+};
+function closepopup() {
+    popUpBox.classList.remove("display");
+};
